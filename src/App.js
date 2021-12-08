1d0c1daa1aadd9cd4f02b436a925c920b5511ae7
Wellington de Lima <wellington.map@hotmail.com>
2021-12-08 11:10:06 -0300

    creating signin page and sign in components, form-input, custom-button and signin components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/Home';
 import ShopPage from './pages/Shop';
+import AuthPage from './pages/Auth';
 
 import Header from './components/Header';
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/auth' component={AuthPage} />
       </Switch>
     </>
   );
---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/SignIn';
+
+import './styles.scss';
+
+const AuthPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default AuthPage;
